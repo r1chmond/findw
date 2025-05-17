0c840ccfeacc745ca5a45516161da8ab90f647be
richmond <richmond.bobie@outlook.com>
2025-05-17 21:47:19 +0100

    parse argument and print to terminal

--- findw.cpp
+++ findw.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
-int main() {
-  std::cout << "Hello world!\n";
+int main(int argc, char *argv[]) {
+  std::cout << "argc: " << argc << "\n";
+  std::cout << "argv: [";
+  for (int i = 0; i < argc; ++i) {
+    std::cout << argv[i] << ", ";
+  }
+  std::cout << "]\n";
   return 0;
 }
