6cac81ef0e0405047219f9bf5f358ffefdb31dc2
richmond <richmond.bobie@outlook.com>
2025-06-21 16:07:28 +0100

    add guards to main

--- findw.cpp
+++ findw.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
+  // guard to enforce parsing 2 arguments
+  if (argc < 3) {
+    std::cerr << "error: insufficient arguments." << std::endl;
+    return 1;
+  } else if (argc > 3) {
+    std::cerr << "error: Too many arguments." << std::endl;
+    return 1;
+  }
+
   std::cout << "argc: " << argc << "\n";
   std::cout << "argv: [";
   for (int i = 0; i < argc; ++i) {
