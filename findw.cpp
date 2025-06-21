5be19d0b4f9d099a963cd4c408c7343f89ce212c
richmond <richmond.bobie@outlook.com>
2025-06-21 18:02:32 +0100

    add pattern search

--- findw.cpp
+++ findw.cpp
@@ -1,40 +1,64 @@
+#include <cstddef>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
+
+using std::cerr;
+using std::cout;
+using std::ifstream;
+using std::string;
+using std::vector;
 
 int main(int argc, char *argv[]) {
   // guard to enforce parsing 2 arguments
   if (argc < 3) {
-    std::cerr << "error: insufficient arguments." << std::endl;
+    cerr << "error: insufficient arguments.\n";
     return 1;
   } else if (argc > 3) {
-    std::cerr << "error: Too many arguments." << std::endl;
+    cerr << "error: Too many arguments.\n";
     return 1;
   }
 
-  std::cout << "argc: " << argc << "\n";
-  std::cout << "argv: [";
+  cout << "argc: " << argc << "\n";
+  cout << "argv: [";
   for (int i = 0; i < argc; ++i) {
-    std::cout << argv[i] << ", ";
+    cout << argv[i] << ", ";
   }
-  std::cout << "]\n";
+  cout << "]\n";
 
-  std::string pattern = argv[1];
-  std::string path = argv[2];
-  std::ifstream infile{path};
+  vector<string> fileContent{};
+  string pattern = argv[1];
+  string path = argv[2];
+  ifstream infile{path};
   if (!infile) {
-    std::cerr << "Error opening file!\n" << "EXIT 1\n";
+    cerr << "Error opening file!\n" << "EXIT 1\n";
     std::exit(EXIT_FAILURE);
   }
   std::string line{};
   while (std::getline(infile, line)) {
-    std::cout << line;
+    fileContent.push_back(line);
   }
-  std::cout << "\n";
+  cout << "\n";
   infile.close();
-  std::cout << "================================ \n";
-  std::cout << "File has successfully been read!";
-  std::cout << "\n";
+  cout << "================================ \n";
+  cout << "File has successfully been read!";
+  cout << "\n";
+
+  string found{};
+  for (const auto &line : fileContent) {
+    size_t pos = line.find(pattern);
+    if (pos != string::npos) {
+      found += line + "\n";
+    }
+  }
+  if (found.empty()) {
+    cout << "\"" + pattern + "\"" + " not found!\n";
+  } else {
+    cout << "\n";
+    cout << "Found!\n";
+    cout << found << "\n";
+  }
 
   return 0;
 }
