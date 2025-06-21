4e376212544b9276958e4e2fcb526a3f5844c35e
richmond <richmond.bobie@outlook.com>
2025-06-21 16:54:11 +0100

    read from file

--- findw.cpp
+++ findw.cpp
@@ -1,4 +1,6 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 
 int main(int argc, char *argv[]) {
   // guard to enforce parsing 2 arguments
@@ -16,5 +18,23 @@ int main(int argc, char *argv[]) {
     std::cout << argv[i] << ", ";
   }
   std::cout << "]\n";
+
+  std::string pattern = argv[1];
+  std::string path = argv[2];
+  std::ifstream infile{path};
+  if (!infile) {
+    std::cerr << "Error opening file!\n" << "EXIT 1\n";
+    std::exit(EXIT_FAILURE);
+  }
+  std::string line{};
+  while (std::getline(infile, line)) {
+    std::cout << line;
+  }
+  std::cout << "\n";
+  infile.close();
+  std::cout << "================================ \n";
+  std::cout << "File has successfully been read!";
+  std::cout << "\n";
+
   return 0;
 }
